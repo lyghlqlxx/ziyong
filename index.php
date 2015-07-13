2a1a65874c1edd24489fff4d29a55d957a4b60eb
真友 <ayhome@zhoucongdeMacBook-Pro.local>
2015-07-13 22:45:38 +0800

    tij

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 define('BIND_MODULE','Admin');
 define('APP_PATH','./Apps/');
+//qidong
 require './PHP/ThinkPHP.php';
